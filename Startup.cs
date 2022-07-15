bd0ef97705ab27d824b297e4c5374511ba673418
JoaoMartins-Forge <joao.martins@autodesk.com>
2022-07-15 09:50:09 -0300

    new tutorial

---
+++ Startup.cs
@@ -0,0 +1,45 @@
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+public class Startup
+{
+  public Startup(IConfiguration configuration)
+  {
+    Configuration = configuration;
+  }
+
+  public IConfiguration Configuration { get; }
+
+  // This method gets called by the runtime. Use this method to add services to the container.
+  public void ConfigureServices(IServiceCollection services)
+  {
+    services.AddControllers();
+    var ForgeClientID = Configuration["FORGE_CLIENT_ID"];
+    var ForgeClientSecret = Configuration["FORGE_CLIENT_SECRET"];
+    var ForgeCallbackURL = Configuration["FORGE_CALLBACK_URL"];
+    if (string.IsNullOrEmpty(ForgeClientID) || string.IsNullOrEmpty(ForgeClientSecret) || string.IsNullOrEmpty(ForgeCallbackURL))
+    {
+      throw new ApplicationException("Missing required environment variables FORGE_CLIENT_ID, FORGE_CLIENT_SECRET, or FORGE_CALLBACK_URL.");
+    }
+  }
+
+  // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+  public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+  {
+    if (env.IsDevelopment())
+    {
+      app.UseDeveloperExceptionPage();
+    }
+    app.UseDefaultFiles();
+    app.UseStaticFiles();
+    app.UseRouting();
+    app.UseEndpoints(endpoints =>
+    {
+      endpoints.MapControllers();
+    });
+  }
+}
