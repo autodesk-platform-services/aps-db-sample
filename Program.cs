bfbacb701dff6205c9990f31592b380e7603bba1
JoaoMartins-Forge <joao.martins@autodesk.com>
2022-11-30 15:12:41 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,16 +1,16 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
-using forge_viewer_db_properties.Hubs;
+using aps_viewer_db_properties.Hubs;
 using Microsoft.AspNetCore.SignalR;
 
 public class Program
 {
   public static void Main(string[] args)
   {
-		var host = CreateHostBuilder(args).Build();
-		var hubContext = host.Services.GetService(typeof(IHubContext<DBHub>));
-		host.Run();
-	}
+    var host = CreateHostBuilder(args).Build();
+    var hubContext = host.Services.GetService(typeof(IHubContext<DBHub>));
+    host.Run();
+  }
 
   public static IHostBuilder CreateHostBuilder(string[] args) =>
       Host.CreateDefaultBuilder(args)
